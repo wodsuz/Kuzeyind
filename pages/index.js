0d83f20d97000933a054e5949c0cad1211179898
ongun demirag <ongun.demirag@gmail.com>
2022-02-22 02:46:06 +0300

    first

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
+  // https://drive.google.com/file/d/1dcYabNhVyYPsRNldU_S9J_EsudiFVG9s/view?usp=sharing
+
   return (
     <div className=" ">
       <Head>
@@ -11,8 +13,9 @@ export default function Home() {
           content="Kuzey ind. export and import company website. Food, food products and disposable medical items"
         />
       </Head>
-      <video autoPlay muted loop className="fixed">
+      <video autoPlay muted loop className="  bg-blue-500">
         <source src="/vid1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
