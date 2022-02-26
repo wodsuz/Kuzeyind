d44c3d1f4d0bd4e2dc71cbb9cc480642a1f26b6c
ongun demirag <ongun.demirag@gmail.com>
2022-02-26 15:07:52 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,51 +1,120 @@
-import React from "react";
-import Image from "next/image";
+import { useState, useEffect, FunctionComponent } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import { useTheme } from "next-themes";
 import { BsFillSunFill, BsMoonFill } from "react-icons/bs";
 
-const Nav = () => {
-  const logo = require("../public/logonew2.jpg");
-  const { theme, setTheme } = useTheme();
-  const changeTheme = () => {
-    console.log(icon);
-    console.log(theme);
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-  const icon =
-    theme === "light" ? (
-      <BsMoonFill className="w-7 h-7 md:w-10 md:h-10" />
-    ) : (
-      <BsFillSunFill className="w-7 h-7 md:w-10 md:h-10" />
-    );
+const NavItem: FunctionComponent<{
+  active: string;
+  setActive: Function;
+  name: string;
+  target: string;
+  route: string;
+}> = ({ active, setActive, name, target, route }) => {
   return (
-    <div>
-      <header className="border-b sticky top-0 z-20 dark:border-0 dark:bg-dark-100 dark:text-primary ">
-        <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
-          <Link href="/" passHref>
-            <a className="cursor-pointer">
-              <Image
-                src={logo}
-                alt={"topicon"}
-                layout="fixed"
-                objectFit="cover"
-                className="rounded-full"
-                height={30}
-                width={30}
-              />
-              <span className="text-md md:text-lg pt-1 font-bold align-top pl-2">
-                Kuzey ind &copy; Order{" "}
-              </span>
-            </a>
-          </Link>
-          <div>
-            <button onClick={changeTheme} aria-label="theme change">
-              {icon}
-            </button>
-          </div>
-        </div>
-      </header>
+    <span>
+      {name === "Order Samples" ? (
+        <Link href={route}>
+          <a target={target}>
+            <span
+              className="mx-2 cursor-pointer border border-blue-500 
+              rounded py-1 px-3 bg-blue-500 text-white
+              hover:bg-blue-300"
+              onClick={() => setActive(name)}
+            >
+              {name}
+            </span>
+          </a>
+        </Link>
+      ) : active !== name ? (
+        <Link href={route}>
+          <a>
+            <span
+              className="mx-2 cursor-pointer hover:border-b-4 hover:border-sky-500"
+              onClick={() => setActive(name)}
+            >
+              {name}
+            </span>
+          </a>
+        </Link>
+      ) : null}
+    </span>
+  );
+};
+/*
+  active !== name ? (
+    <Link href={route}>
+      <a>
+        <span
+          className="mx-2 cursor-pointer hover:border-b-4 hover:border-sky-500"
+          onClick={() => setActive(name)}
+        >
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
+*/
+const Nav = () => {
+  const { pathname } = useRouter();
+  const [active, setActive] = useState("");
+  const logo = require("../public/logonew2.jpg");
+
+  useEffect(() => {
+    if (pathname === "/") setActive("Home");
+    else if (pathname === "/food") setActive("Food Products");
+    else if (pathname === "/medical") setActive("Medical Products");
+    else if (pathname === "/aboutus") setActive("About Kuzey");
+  }, [pathname]);
+  return (
+    <div className="flex items-center justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 md:text-2xl border-sky-500">
+        {active}
+      </span>
+      <div className="text-base font-normal md:text-xl">
+        <NavItem active={active} setActive={setActive} name="Home" route="/" />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Food Products"
+          route="/food"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Medical Products"
+          route="/medical"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="About Kuzey"
+          route="/aboutus"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="FAQ"
+          route="/faq"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Contact"
+          route="/contact"
+        />
+        <NavItem
+          active="Home"
+          setActive={setActive}
+          name="Order Samples"
+          target="_blank"
+          route="https://kuzeyind-sample-order.vercel.app/"
+        />
+      </div>
     </div>
   );
 };
+
 export default Nav;
